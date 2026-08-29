f23d95143549f968ed4f99bfb7dfa5d29ab85204
Mannn07 <127299958+Mannn07@users.noreply.github.com>
2026-08-29 11:46:56 +0700

    week01: add calculateStatus function

--- src/oop_00000066420_ImanuelGilland/week01/GradingSystem.kt
+++ src/oop_00000066420_ImanuelGilland/week01/GradingSystem.kt
@@ -1,11 +1,24 @@
 package oop_00000066420_ImanuelGilland.week01
 
+// Function di luar main()
+fun calculateStatus(score: Int) = if (score > 75) "Lulus" else "Tidak Lulus"
+
 fun main() {
 
-    // REFACTOR: Gunakan val dan hapus tipe data eksplisit
     val name = "John Thor"
     val score = 80
 
-    // REFACTOR: String Template ($name)
     println("Nama: $name, Nilai: $score")
+
+    val grade = when (score) {
+        in 90..100 -> "A"
+        in 80..89 -> "B"
+        in 70..79 -> "C"
+        else -> "D"
+    }
+
+    println("Grade kamu: $grade")
+
+    // Panggil function di dalam main()
+    println("Status: ${calculateStatus(score)}")
 }
