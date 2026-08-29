447cbfdd57ea4fda44a6aed8d2413d4bd30a3b3c
Mannn07 <127299958+Mannn07@users.noreply.github.com>
2026-08-29 12:02:39 +0700

    week01: (tugas 1) clean up unused argos and comments

--- src/oop_00000066420_ImanuelGilland/week01/CircleCalculator.kt
+++ src/oop_00000066420_ImanuelGilland/week01/CircleCalculator.kt
@@ -1,21 +1,15 @@
 package oop_00000066420_ImanuelGilland.week01
 
-fun main(args: Array<String>) {
-    // Variable definition
+fun main() {
     val radius = 7.0
     val pi = 3.14
 
-    // Calculation
     val area = pi * radius * radius
 
-    // Output menggunakan String Template
     println("Radius: $radius, Area: $area")
-
-    // Panggil checkSize di dalam println
     println(checkSize(area))
 }
 
-// Expression Body Function
 fun checkSize(area: Double) =
     if (area > 100) "This is a Big Circle"
     else "This is a Small Circle"
