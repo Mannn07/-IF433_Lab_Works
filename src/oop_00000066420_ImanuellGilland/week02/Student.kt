6e30c5b9485d7806bd2c829dd74d08bddb435070
Mannn07 <127299958+Mannn07@users.noreply.github.com>
2026-09-05 08:11:23 +0700

    week02: challenge refactor primary constructor using default args

--- src/oop_00000066420_ImanuellGilland/week02/Student.kt
+++ src/oop_00000066420_ImanuellGilland/week02/Student.kt
@@ -3,7 +3,8 @@ package oop_00000066420_ImanuellGilland.week02
 class Student(
     val name: String,
     val nim: String,
-    var major: String
+    var major: String,
+    var gpa: Double = 0.0
 ) {
     init {
         // Validasi Sederhana: Cek panjang NIM
