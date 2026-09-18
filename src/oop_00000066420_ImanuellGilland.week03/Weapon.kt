2f893831d6b0d5b87f5ed34191b7bb225194191a
Mannn07 <127299958+Mannn07@users.noreply.github.com>
2026-09-19 00:42:00 +0700

    week03: (task 1) create Weapon class with validation

---
+++ src/oop_00000066420_ImanuellGilland.week03/Weapon.kt
@@ -0,0 +1,23 @@
+package oop_00000066420_ImanuellGilland.week03
+
+class Weapon(val name: String) {
+
+    var damage: Int = 0
+        set(value) {
+            if (value < 0) {
+                println("Peringatan: Damage tidak boleh negatif!")
+                // Nilai field tidak diubah
+            } else if (value > 1000) {
+                field = 1000 // Paksa nilai menjadi 1000
+            } else {
+                field = value
+            }
+        }
+
+    val tier: String
+        get() = when {
+            damage > 800 -> "Legendary"
+            damage > 500 -> "Epic"
+            else -> "Common"
+        }
+}
